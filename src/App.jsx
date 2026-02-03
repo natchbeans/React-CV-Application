0c3f099c4298a8c182d1f3652023bfcf17d87070
natchbeans <natchbeans@gmail.com>
2026-02-03 14:09:10 -0600

    Created Next and Previous buttons to go to 'next page', depending on the currentPage state, I will render HTML for that component. So currentPage=1 renders the HTML for GeneralInfo, if currentPage=2, the HTML renders for component of School Info.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { useState } from 'react';
 
 export default function App(){
 
+  const [currentPage, setCurrentPage] = useState(1);
+
   const [generalInfo, setGeneralInfo] = useState({
     name: "",
     email: "",
@@ -25,7 +27,13 @@ export default function App(){
     endDate: "",
   }); 
 
-  
+  function goNextPage(){
+    setCurrentPage(currentPage + 1); //So 1 goes to 2, and 2 goes to 3...
+  }
+
+  function goPreviousPage(){
+    setCurrentPage(currentPage - 1); //3 -> 2, 2 -> 1
+  }
 
   function saveCompleteCV(){
       console.log("Complete CV:", {
@@ -40,11 +48,32 @@ export default function App(){
 
   return (
     <div>
-      <GeneralInfo data={generalInfo} onSave={setGeneralInfo}/>
-      <EducationExp data={educationExp} onSave={setEducationExp}/>
-      <AddExp data={jobInfo} onSave={setJobInfo}/>
 
-      <button onClick={saveCompleteCV}>Save Complete CV</button>
+      {/* Curly braces in JSX are used to embed JS, if currentPage is 1,2,3 then render HTML */}
+      {currentPage === 1 && (
+        <div>
+          <GeneralInfo data={generalInfo} onSave={setGeneralInfo}/>
+          <button onClick={goNextPage}>Next Page</button>
+        </div>
+      )}
+
+      {currentPage === 2 && (
+        <div>
+          <EducationExp data={educationExp} onSave={setEducationExp}/>
+          <button onClick={goPreviousPage}>Back</button>
+          <button onClick={goNextPage}>Next Page</button>
+        </div>
+      )}
+      
+      {currentPage === 3 && (
+        <div>
+          <AddExp data={jobInfo} onSave={setJobInfo}/>
+          <button onClick={goPreviousPage}>Back</button>
+          <button onClick={saveCompleteCV}>Save Complete CV</button>
+        </div>
+      )}
+      
+      
     </div>
     
   );
